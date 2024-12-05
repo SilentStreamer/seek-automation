ec4056dee95bedb152b046c687883716e6637f18
Jacob1802 <jacobrdilonardo@gmail.com>
2024-12-06 09:51:25 +1100

    improved error handling

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -2,8 +2,14 @@
 from apify_client import ApifyClient
 from dotenv import load_dotenv
 from typing import Dict, List
+import logging
 import os
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
 class JobScraper:
     def __init__(self, run_config: Dict):
@@ -11,9 +17,13 @@ class JobScraper:
         self.client = ApifyClient(os.getenv("APIFY_KEY"))
         
     def scrape(self, actor: str) -> List[Dict]:
-        run = self.client.actor(actor).call(run_input=self.run_config)
-        return self.get_dataset(run)
-    
-    def get_dataset(self, run: Dict) -> List[Dict]:
+        try:
+            run = self.client.actor(actor).call(run_input=self.run_config)
+            return self._get_dataset(run)
+        except Exception as e:
+            logging.error(e)
+            return [{}]
+
+    def _get_dataset(self, run: Dict) -> List[Dict]:
         data = self.client.dataset(run["defaultDatasetId"]).list_items().items
         return data
